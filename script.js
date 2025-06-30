3e57414b1f3e04433cb28426b19f3440345785db
Vivek Pandey <45vive@gmail.com>
2025-06-30 21:38:03 +0530

    Payment third commit

--- script.js
+++ script.js
@@ -8,3 +8,6 @@ const footer = "Footer added in our website";
 console.log(footer);
 const Payment = "Integrated the payment gateway";
 console.log(Payment);
+
+const upi = "Integrated the upi payment system";
+console.log(upi);
